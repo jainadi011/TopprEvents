bccef54e7542b277a524acba42d685ec1ae900ca
aditya.96444 <ADITYAja@27#>
2016-09-24 13:22:29 +0530

    Refactored project for MVP architecture

--- app/src/main/java/com/zriton/topprevents/view/activity/MainActivity.java
+++ app/src/main/java/com/zriton/topprevents/view/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.zriton.topprevents;
+package com.zriton.topprevents.view.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.zriton.topprevents.R;
 
 public class MainActivity extends AppCompatActivity {
 
