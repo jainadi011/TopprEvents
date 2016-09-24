500176f359ce0fcc8cda22d412cfd2f9d4671418
aditya.96444 <ADITYAja@27#>
2016-09-24 13:48:10 +0530

    Added fragments for tabs

---
+++ app/src/main/java/com/zriton/topprevents/view/fragment/EventsFragment.java
@@ -0,0 +1,10 @@
+package com.zriton.topprevents.view.fragment;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by aditya on 24/09/16.
+ */
+
+public class EventsFragment extends Fragment {
+}
