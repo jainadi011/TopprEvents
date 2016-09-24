88725684d1a6f097e4e6fda79dbb6236d8c92a8b
aditya.96444 <ADITYAja@27#>
2016-09-24 15:29:35 +0530

    Added APP constants

---
+++ app/src/main/java/com/zriton/topprevents/util/Config.java
@@ -0,0 +1,16 @@
+package com.zriton.topprevents.util;
+
+/**
+ * Created by aditya on 24/09/16.
+ */
+
+public class Config {
+
+    public static final String MAIN_URL = "https://hackerearth.0x10.info/api/";
+
+    public static final String GET_EVENTS = "toppr_events";
+    public static final String GET_EVENTS_TYPE_KEY = "type";
+    public static final String GET_EVENTS_TYPE_VALUE = "json";
+    public static final String GET_EVENTS_QUERY_KEY = "query";
+    public static final String GET_EVENTS_QUERY_VALUE = "list_events";
+}
