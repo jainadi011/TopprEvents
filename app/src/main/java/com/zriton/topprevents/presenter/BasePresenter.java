a305e61ccb6d684f55e66b86e758dd5fc78c572e
aditya.96444 <ADITYAja@27#>
2016-09-24 13:30:13 +0530

    Added base presenter and base view

---
+++ app/src/main/java/com/zriton/topprevents/presenter/BasePresenter.java
@@ -0,0 +1,13 @@
+package com.zriton.topprevents.presenter;
+
+/**
+ * Created by aditya on 24/09/16.
+ */
+
+public interface BasePresenter {
+
+    void subscribe();
+
+    void unsubscribe();
+
+}
---
+++ app/src/main/java/com/zriton/topprevents/presenter/BaseView.java
@@ -0,0 +1,11 @@
+package com.zriton.topprevents.presenter;
+
+/**
+ * Created by aditya on 24/09/16.
+ */
+
+public interface BaseView<T> {
+
+    void setPresenter(T presenter);
+
+}
